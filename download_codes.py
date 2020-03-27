b187e3b64dec3dd4a47815857e31c95565b81dac
ash2703 <nahtnam2703@gmail.com>
2020-03-27 21:37:52 +0530

    Download files from link in XL sheet concurrently

--- download_codes.py
+++ download_codes.py
@@ -14,10 +14,12 @@ logger.setLevel(logging.DEBUG)
 
 
 def unzip_files(file_name, date, name):
+    '''Unzip and save the downloaded files sorted by date'''
+
     try:
         # opening the zip file in READ mode 
         with ZipFile(file_name, 'r') as zip:    
-            zip.extractall(path = f'Downloads\\Non Contest\\{date}\\{name}//')   # extracting all the files 
+            zip.extractall(path = f'Downloads\\Contest\\{date}\\{name}//')   # extracting all the files 
 
     except Exception as e:
         # print(f"{name} not valid file")
@@ -25,10 +27,12 @@ def unzip_files(file_name, date, name):
 
 
 def download_files(name, date, link):
+    '''Dowload and save zip files to a folder from given link'''
+
     try:
-        # print("downloading {}".format(name))
+        print("downloading {}".format(name))
         data = requests.get(link).content
-        file_name = f'Downloads\\Non Contest\\Zip\\{name}.zip'
+        file_name = f'Downloads\\Contest\\Zip\\{name}.zip'
         with open(file_name, 'wb') as _file:
             _file.write(data)
             
@@ -37,17 +41,18 @@ def download_files(name, date, link):
     except Exception as e:
         logger.warn(f"{name} not downloadable") 
 
-path = "E:\Codes\Python\Web-Automate-Selenium\Internshala-Python-Users.xlsx"
+path = "E:\Codes\Python\Web-Automate-Selenium\Internshala-Python-Users.xlsx"  #XL sheet path
 book = openpyxl.load_workbook(path)
-sheet = book.get_sheet_by_name("non_contest_users")
+sheet = book.get_sheet_by_name("contest_users")   #fetch all user data
 
 names = [name.value for name in sheet["A"][1:]]
 dates = [date.value.split(" ")[0] for date in sheet["C"][1:]]
 links = [link.value for link in sheet["D"][1:]]
 
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
+with concurrent.futures.ThreadPoolExecutor() as executor:   #Concurrently download all files usuing multithreading
 	results = executor.map(download_files , names, dates, links)
+count = 0
 for result in results:
-    print(result)
-
+    count +=1
+    print(result,"  ", count)
